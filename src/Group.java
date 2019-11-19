b54831c96ddeeb022783f291f44b08ea55a02a6d
trippt1c <dylantrang1234@gmail.com>
2019-11-19 10:53:01 -0600

    created classes

---
+++ src/Group.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Tripptic on 11/19/2019.
+ * Project: PromPTing
+ * Good Luck
+ */
+/*
+This file was made by dylan
+*/
+public class Group {
+
+    private int ID;
+    private ArrayList<Student> students;
+
+    public Group() {
+        ID = 0;
+        students = new ArrayList<Student>();
+    }
+
+    public Group(int i) {
+        ID = i;
+        students = new ArrayList<Student>();
+    }
+
+    public void addStudent(Student s) {
+        students.add(s);
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public int size() {
+        return students.size();
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Tripptic on 11/19/2019.
+ * Project: PromPTing
+ * Good Luck
+ */
+/*
+This file was made by dylan
+*/
+public class Student {
+    private String Name;
+    private String ID;
+    private int groupId;
+
+    public Student(String n, String i, int g)
+    {
+      Name = n;
+      ID = i;
+      groupId = g;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+}

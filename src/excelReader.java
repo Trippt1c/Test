677a211a3d278af3a02634f97b3654c574af2c9b
trippt1c <dylantrang1234@gmail.com>
2019-11-19 10:11:47 -0600

    oneLiner test

--- src/excelReader.java
+++ src/excelReader.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by Tripptic on 11/19/2019.
  * Project: PromPTing
@@ -8,6 +11,7 @@ This file was made by dylan
 */
 public class excelReader {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int[] hm = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
     }
 }
